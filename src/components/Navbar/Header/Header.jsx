22c25d5d7f4d3eeafbd25d44de46a77f8d5cdaa0
saurabh Nishad <saurabhnishad.7651@gmail.com>
2025-02-02 11:43:49 +0530

    working on header

--- src/components/Navbar/Header/Header.jsx
+++ src/components/Navbar/Header/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import "./Header.css"
+import React from 'react';
+import "./Header.css";
+
 const Header = () => {
   return (
     <div>
-      
+      <div className="header" >
+        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, quos?</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime exercitationem ad, repudiandae esse aliquam iure dignissimos ipsum itaque distinctio facere expedita ducimus placeat explicabo dolor rem saepe sit eum magnam?</p>
+        <button>View Menu</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
