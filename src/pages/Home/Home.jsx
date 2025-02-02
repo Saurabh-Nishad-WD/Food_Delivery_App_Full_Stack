c696f1e48c36a7b616b375b3fb718fb5c04261e9
saurabh Nishad <saurabhnishad.7651@gmail.com>
2025-02-02 15:07:21 +0530

    working on food items

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./Home.css"
 import Header from '../../components/Navbar/Header/Header'
+import ExploreMenu from '../../components/exploreMenu/ExploreMenu'
 const Home = () => {
   return (
     <div className='home'>
-      <Header/>
+      <Header className="header"/>
+     <div className="exploreMenu">
+     <ExploreMenu />
+     </div>
     </div>
   )
 }
